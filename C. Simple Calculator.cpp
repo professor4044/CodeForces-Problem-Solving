ada41b8d7a4408893f5284acf9bf5c3581cdfb47
Md. Jamir Sheikh <mejamirsheikh@gmail.com>
2026-04-27 23:41:11 +0600

    Add files via upload

---
+++ C. Simple Calculator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    long long x,y;
+    cin>> x>>y;
+
+    long long sum = x + y ;
+    long long mul = x * y;
+    long long sub = x - y;
+
+    cout << x << " "<< "+" << " " << y << " " << "=" << " " << sum << endl;
+    cout << x << " * " << y << " = " << mul << endl;
+    cout << x << " - " << y << " = " << sub << endl;
+}
---
+++ D. Difference.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    long long A,B,C,D;
+    //long long A,B,C,D;
+
+    cin>> A>>B>>C>>D;
+
+    long long mul = A * B;
+    long long mul2 = C * D;
+    long long sub = (A * B) - (C * D);
+
+   //long long X = (A*B)-(C*D);
+
+    cout << "Difference = "<< sub << endl;
+
+
+}
