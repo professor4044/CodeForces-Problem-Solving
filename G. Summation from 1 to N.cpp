fdb81923f32ea89615bbfc92cbbfb2c206bc691e
Md. Jamir Sheikh <mejamirsheikh@gmail.com>
2026-04-28 20:54:40 +0600

    Add files via upload

---
+++ G. Summation from 1 to N.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long long n;
+    int i=1;
+
+    cin>> n;
+    long long result=n*(i+n)/2;
+
+    //for(i=1;i<=n;i++){
+        //result = result+i;
+
+
+    cout<< result <<endl;
+
+}
