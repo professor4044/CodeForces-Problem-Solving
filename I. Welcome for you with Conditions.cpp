4b8085258cf9efc9d5029792ef7eb8fc34559303
Md. Jamir Sheikh <mejamirsheikh@gmail.com>
2026-04-29 20:13:46 +0600

    Add files via upload

---
+++ I. Welcome for you with Conditions.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+
+    if (A > B || A >= B ){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" <<endl;
+    }
+}
